5f9280b2c3820ab9ae22df7fbf784d1f2dbce3f6
HilaIlarov <hilailarov@gmail.com>
2022-03-31 01:21:32 +0300

    App.js+package.json

--- App.js
+++ App.js
@@ -2,22 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Button } from "react-native";
 import HomeScreen from "./app/screens/HomeScreen";
 import ProfileMatching from "./app/screens/ProfileMatching";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ChooseActivity from "./app/screens/ChooseActivity";
+import WelcomeBackScreen from "./app/screens/WelcomeBackScreen";
+import MatchesScreen from "./app/screens/MatchesScreen";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      {/* <HomeScreen /> */}
-      <ProfileMatching />
+    <View>
+      <ChooseActivity />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
