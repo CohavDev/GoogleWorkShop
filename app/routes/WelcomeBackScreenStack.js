bf4ce9edb962c36218fe58f0ffcf706e76dba822
CohavDev <shaulspam@gmail.com>
2022-04-04 20:10:27 +0300

    added screens to route in welcomebackstack

--- app/routes/WelcomeBackScreenStack.js
+++ app/routes/WelcomeBackScreenStack.js
@@ -5,7 +5,11 @@ import {
 import { createAppContainer } from "react-navigation";
 import MainMenu from "../screens/MainMenu";
 import ChooseActivity from "../screens/ChooseActivity";
+import NewActivity from "../screens/NewActivity";
 import MyActivities from "../screens/MyActivities";
+import ProfileMatching from "../screens/ProfileMatching";
+import MatchesScreen from "../screens/MatchesScreen";
+import ApproveActivity from "../screens/ApproveActivity";
 
 const screens = {
   MainMenu: {
@@ -16,6 +20,18 @@ const screens = {
   },
   MyActivities: {
     screen: MyActivities,
+  },
+  ProfileMatching: {
+    screen: ProfileMatching,
+  },
+  MatchesScreen: {
+    screen: MatchesScreen,
+  },
+  NewActivityForm: {
+    screen: NewActivity,
+  },
+  ApproveActivity: {
+    screen: ApproveActivity,
   },
 };
 
