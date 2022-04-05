b2be5dda94d42305f3ffb1c462678064937fcf8d
CohavDev <shaulspam@gmail.com>
2022-04-05 11:18:16 +0300

    added myactivites the 'flatlist'-component

--- app/screens/MyActivities.js
+++ app/screens/MyActivities.js
@@ -1,4 +1,11 @@
-import { StyleSheet, Text, View, ScrollView, Pressable } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Pressable,
+  FlatList,
+} from "react-native";
 import React from "react";
 import ActivityItem from "../components/ActivityItem";
 import { Entypo } from "@expo/vector-icons";
@@ -6,38 +13,72 @@ import myColors from "../config/colors";
 
 const DATA = [
   {
+    key: "1",
     activityIcon: "drink",
     activityName: "Drink",
     location: "Tel- Aviv",
     date: "07/05/2022",
   },
   {
+    key: "2",
     activityIcon: "bowl",
     activityName: "Restaurant",
     location: "Paris",
     date: "01/08/2022",
   },
   {
+    key: "3",
     activityIcon: "moon",
     activityName: "Place To Sleep",
     location: "Tel- Aviv",
     date: "02/06/2022",
   },
   {
+    key: "4",
     activityIcon: "drink",
     activityName: "Drink",
     location: "New York",
     date: "12/04/2022",
   },
+  // {
+  //   key: "5",
+  //   activityIcon: "drink",
+  //   activityName: "Drink",
+  //   location: "New York",
+  //   date: "12/04/2022",
+  // },
+  // {
+  //   key: "6",
+  //   activityIcon: "drink",
+  //   activityName: "Drink",
+  //   location: "New York",
+  //   date: "12/04/2022",
+  // },
 ];
+const renderItem = ({ item }) => (
+  <ActivityItem
+    activityIcon={item.activityIcon}
+    activityName={item.activityName}
+    date={item.date}
+    location={item.location}
+  />
+);
 export default function MyActivities({ navigation }) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.title}>My Current Activities</Text>
       </View>
-      <ScrollView>
-        <View style={[styles.container, { paddingHorizontal: 15 }]}>
+      {/* <ScrollView> */}
+      <View style={[styles.container, { paddingHorizontal: 15 }]}>
+        <FlatList
+          data={DATA}
+          keyExtractor={(item) => item.key}
+          renderItem={renderItem}
+        />
+      </View>
+
+      {/* <View style={[styles.container, { paddingHorizontal: 15 }]}>
           <ActivityItem {...{ activityIcon: "drink", activityName: "Drink" }} />
           <ActivityItem
             {...{ activityIcon: "bowl", activityName: "Restaurant" }}
@@ -46,8 +87,8 @@ export default function MyActivities({ navigation }) {
             {...{ activityIcon: "moon", activityName: "Place to Sleep" }}
           />
           <ActivityItem {...{ activityIcon: "drink", activityName: "Drink" }} />
-        </View>
-      </ScrollView>
+        </View> */}
+      {/* </ScrollView> */}
       <Pressable
         android_ripple={{ color: "white" }}
         style={[myColors.circularImage, { marginHorizontal: 15 }]}
