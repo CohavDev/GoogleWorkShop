d9b75e6e1599a94d7faa156fd98a7b0730880c8f
OmerNaveh7 <102812776+OmerNaveh7@users.noreply.github.com>
2022-08-15 17:51:25 +0300

    correction

--- App.js
+++ App.js
@@ -26,6 +26,7 @@ import ChooseOutdoorsActivity from "./app/screens/ChooseOutdoorsActivity";
 import ChooseIndoorsActivity from "./app/screens/ChooseIndoorsActivity";
 import HomeScreen from "./app/screens/HomeScreen";
 
+
 import LandPage from "./app/screens/LandPage";
 import ActivitiesScreen from "./app/screens/ActivitiesScreen";
 import OccurringActivityPreview from "./app/screens/OccurringActivityPreview";
@@ -36,7 +37,6 @@ import { IconButton, Colors } from "react-native-paper";
 import { I18nManager } from "react-native"; // force left to right layout of app
 import { firebase } from "./app/firebase/config";
 import colors from "./app/config/colors";
-import { getAuth, signOut } from "firebase/auth";
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -99,17 +99,6 @@ export default function App() {
 }
 
 function TabsNav(props) {
-  // this function logs user out, and navigates to login-screen
-  const logoutFunc = () => {
-    const auth = getAuth();
-    signOut(auth)
-      .then(() => {
-        props.navigation.popToTop();
-        props.navigation.navigate("LoginScreen");
-        // navigate to login-screen
-      })
-      .catch((error) => alert(error));
-  };
   return (
     <Tab.Navigator
       screenOptions={{
@@ -182,19 +171,12 @@ function TabsNav(props) {
       />
 
       <Tab.Screen
-        name="logout"
+        name="settings"
         component={HomeScreen}
-        listeners={{
-          tabPress: (e) => {
-            e.preventDefault();
-            // logout current user
-            logoutFunc();
-          },
-        }}
         options={{
           tabBarIcon: ({ focused }) => (
             <IconButton
-              icon="logout"
+              icon="cog-outline"
               // color="#BFD9CD"
               color={colors.TabBarText}
               // size={12}
@@ -258,7 +240,10 @@ function MainNavigation(props) {
         component={BubblesCategories}
       />
       <Stack.Screen name="ApproveActivity" component={ApproveActivity} />
-      <Stack.Screen name="ActivitiesScreen" component={ActivitiesScreen} />
+      <Stack.Screen
+        name="ActivitiesScreen"
+        component={ActivitiesScreen}
+      />
       <Stack.Screen name="NewActivityForm" component={NewActivityForm} />
     </Stack.Navigator>
   );
--- app/screens/ProfileMatching.js
+++ app/screens/ProfileMatching.js
@@ -97,13 +97,10 @@ export default function ProfileMatching(props) {
     console.log("Render button called =" + matched_status);
     // render 'match with' text / 'pending' text / whatsapp icon according to matchedActivityStatus
     // var matched_status = activityData.matchedActivityStatus;
-    const status_both = matched_status.localeCompare("accepted by both") == 0;
-    const status_other =
-      matched_status.localeCompare("accepted only by other user") == 0;
-    const status_only_me =
-      matched_status.localeCompare("accepted only by me") == 0;
-    const status_none =
-      matched_status.localeCompare("accepted by non of us") == 0;
+    const status_both = (matched_status.localeCompare("accepted by both") == 0);
+    const status_other = (matched_status.localeCompare("accepted only by other user") == 0);
+    const status_only_me = (matched_status.localeCompare("accepted only by me") == 0);
+    const status_none = (matched_status.localeCompare("accepted by non of us") == 0);
     if (status_other) {
       return (
         <View style={styles.buttonContainer}>
@@ -116,19 +113,18 @@ export default function ProfileMatching(props) {
               console.log("pressed");
               allActivitiesRef.doc(otherUserData.activityDocID).update({
                 travelPartnersIDs:
-                  firebase.firestore.FieldValue.arrayUnion(userID),
-              });
+                    firebase.firestore.FieldValue.arrayUnion(userID)
+              })
               allActivitiesRef.doc(otherUserData.activityDocID).update({
-                matchedActivityID: thisUserData.activityDocID,
-              });
+                matchedActivityID: thisUserData.activityDocID
+              })
               allActivitiesRef.doc(otherUserData.activityDocID).update({
-                status: "paired",
-              });
+                status: "paired"
+              })
               allActivitiesRef.doc(thisUserData.activityDocID).update({
-                travelPartnersIDs: firebase.firestore.FieldValue.arrayUnion(
-                  otherUserData.userID
-                ),
-              });
+                travelPartnersIDs:
+                    firebase.firestore.FieldValue.arrayUnion(otherUserData.userID)
+              })
               allActivitiesRef.doc(thisUserData.activityDocID).update({
                 matchedActivityID: otherUserData.activityDocID,
               });
