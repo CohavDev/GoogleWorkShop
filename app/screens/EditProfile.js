ee990448644199442cb6a7c036489461384f71f0
CohavDev <shaulspam@gmail.com>
2022-08-18 17:38:36 +0300

    EditProfile added (UI & firebase)

--- app/screens/EditProfile.js
+++ app/screens/EditProfile.js
@@ -1,105 +1,113 @@
-import {
-    StyleSheet,
-    Text,
-    View,
-    Image,
-    Button,
-    TouchableOpacity,
-    TextInput
-  } from "react-native";
-  import { AntDesign } from "@expo/vector-icons";
-  import React, { useEffect, useState } from "react";
-  import { firebase } from "../firebase/config.js";
-  import myColors from "../config/colors";
-  import { Pressable, Vibration } from "react-native";
-  import { LinearGradient } from "expo-linear-gradient";
-  import { IconButton } from "react-native-paper";
-  import * as Linking from "expo-linking";
-  import colors from "../config/colors";
-  import { Picker } from "@react-native-picker/picker";
+import React, { useEffect, useState } from "react";
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import styles from "../firescreens/RegistrationScreen/styles";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import { firebase } from "../firebase/config";
 
+export default function EditProfile(props) {
+  //page states
+  const [fullName, setFullName] = useState("");
+  const [location, setLocation] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState();
+  const [aboutMe, setAboutMe] = useState("");
+  const [isRendered, setRender] = useState(false);
+  //lcoal variables
+  const userID = firebase.auth().currentUser.uid;
+  const userRef = firebase.firestore().collection("users");
+  //functions
+  const onPressSave = () => {
+    userRef
+      .doc(userID)
+      .update({
+        fullName: fullName,
+        phoneNumber: phoneNumber,
+        nationality: location,
+        aboutMe: aboutMe,
+      })
+      .then(() => {
+        props.route.params.onGoBack();
+        props.navigation.goBack();
+      });
+  };
+  useEffect(() => {
+    if (isRendered) {
+      return;
+    }
+    userRef
+      .doc(userID)
+      .get()
+      .then((userData) => {
+        const data = userData.data();
+        console.log("fetched data for edit profile");
+        setRender(true);
+        setFullName(data.fullName);
+        setAboutMe(data.aboutMe);
+        setPhoneNumber(data.phoneNumber);
+        setLocation(data.nationality);
+      });
+  });
+  return (
+    <View style={styles.container}>
+      <KeyboardAwareScrollView
+        style={{ flex: 1, width: "100%" }}
+        keyboardShouldPersistTaps="always"
+      >
+        <Text style={{ fontSize: 18, alignSelf: "center" }}>
+          Edit your profile
+        </Text>
+        {/* <Image
+          style={styles.logo}
+          source={require("../../../app/assets/TravelPartnerLogo1.jpg")}
+        /> */}
 
-  export default function EditProfile(props) {
-    console.log(props.route.params);
-	const DATA = {
-        //user data to be edited
-	  aboutMe: props.route.params.aboutMe,
-	  fullName: props.route.params.fullName,
-	  nationality: props.route.params.nationality,
-	  nativeLanguage: props.route.params.nativeLanguage,
-	  secondLanguage: props.route.params.secondLanguage,
-	  phoneNumber: props.route.params.phoneNumber,
-	  age: props.route.params.age,
-	};
-
-    const userID = firebase.auth().currentUser.uid;
-    const allActivitiesRef = firebase.firestore().collection("allActivities");
-    const userRef = firebase.firestore().collection("users").doc(userID);
-    const [aboutMe , setAboutMe] = useState("")
-    const [fullName , setFullName] = useState("")
-    const [nationality , setNationality] = useState("")
-    const [nativeLanguage , setNativeLanguage] = useState("")
-    const [secondLanguage , setSecondLanguage] = useState("")
-    const [phoneNumber , setPhoneNumber] = useState("")
-    const [formattedDateOfBirth , setformattedDateOfBirth] = useState("")
-    const userData = 
-        {
-            aboutMe: aboutMe,
-            fullName: fullName,
-            nationality: nationality,
-            nativeLanguage: nativeLanguage,
-            secondLanguage: secondLanguage,
-            phoneNumber: phoneNumber,
-            age: getAge(formattedDateOfBirth)
-        }
-        const onClickAprrove = () => {
-            userRef.get().then((result) => {
-              const userFormattedDateOfBirth = result.data().formattedDateOfBirth;
-              const timestamp = firebase.firestore.FieldValue.serverTimestamp();
-              day = DATA.startDate.slice(0, 2);
-              month = DATA.startDate.slice(3, 5);
-              year = DATA.startDate.slice(6, 10);
-              // i assume the format of the date of birth is : DD/MM/YYYY
-              const formattedStartDate = parseInt("".concat(year, month, day));
-              day = DATA.endDate.slice(0, 2);
-              month = DATA.endDate.slice(3, 5);
-              year = DATA.endDate.slice(6, 10);
-              // i assume the format of the date of birth is : DD/MM/YYYY
-              const formattedEndDate = parseInt("".concat(year, month, day));
-              const activityData = {
-                userID: userID,
-                createdAt: timestamp,
-                type: DATA.type,
-                startDate: DATA.startDate,
-                formattedStartDate: formattedStartDate,
-                endDate: DATA.endDate,
-                formattedEndDate: formattedEndDate,
-                time: DATA.time,
-                userFormattedDateOfBirth: userFormattedDateOfBirth,
-                location: DATA.location,
-                languages: languagesArray,
-                travelPartnersIDs: travelPartnersIDs,
-                matchedActivityID: matchedActivityID,
-                status: status,
-              };
-    
-        function getAge(formattedDateOfBirth) {
-            let dateOfBirth = formattedDateOfBirth.toString();
-        
-            var day = dateOfBirth.slice(6, 8);
-            var month = dateOfBirth.slice(4, 6);
-            var year = dateOfBirth.slice(0, 4);
-            // i assume the format of the date of birth is : DD/MM/YYYY
-            dateOfBirth = "".concat(year, "-", month, "-", day);
-            var ageInMilliseconds = new Date() - new Date(dateOfBirth);
-            return Math.floor(ageInMilliseconds / 1000 / 60 / 60 / 24 / 365); // convert to years
-          }
-    
-          return (
-              <View>
-                  
-              </View>
-          )
-
-
-  }
+        <TextInput
+          style={styles.input}
+          placeholder="Full Name"
+          placeholderTextColor="#aaaaaa"
+          value={fullName}
+          onChangeText={(text) => setFullName(text)}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Where are you from?"
+          placeholderTextColor="#aaaaaa"
+          value={location}
+          onChangeText={(text) => setLocation(text)}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          placeholder="Phone Number"
+          value={phoneNumber}
+          onChangeText={(text) => setPhoneNumber(text)}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#aaaaaa"
+          placeholder="About me"
+          value={aboutMe}
+          onChangeText={(text) => setAboutMe(text)}
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity style={styles.button} onPress={() => onPressSave()}>
+          <Text style={styles.buttonTitle}>Save changes</Text>
+        </TouchableOpacity>
+        {/* <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Already got an account?{" "}
+            <Text onPress={onFooterLinkPress} style={styles.footerLink}>
+              Log in
+            </Text>
+          </Text>
+        </View> */}
+      </KeyboardAwareScrollView>
+    </View>
+  );
+}
