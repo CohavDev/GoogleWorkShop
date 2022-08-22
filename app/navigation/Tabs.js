c3096542ac247c87bda48f4a2f29c0b31d0ea1b1
OmerNaveh7 <102812776+OmerNaveh7@users.noreply.github.com>
2022-08-22 17:23:01 +0300

    cleaning nevigation tabs

--- app/navigation/Tabs.js
+++ app/navigation/Tabs.js
@@ -3,21 +3,13 @@ import { StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ProfileMatching from "../screens/ProfileMatching";
 import MyActivities from "../screens/MyActivities";
-import LoginScreen from "../firescreens/LoginScreen/LoginScreen";
 
-import { View, Text } from "react-native";
-import { IconButton, Colors } from "react-native-paper";
+import { IconButton } from "react-native-paper";
 import { HomeScreen } from "../firescreens";
 
 const Tab = createBottomTabNavigator();
 function Tabs(props) {
-  console.log("reached here");
   return (
-    // <View>
-    //     <Text>
-    //         hello
-    //     </Text>
-    // </View>
     <Tab.Navigator
       tabBarOptions={{
         showLabel: false,
@@ -39,9 +31,7 @@ function Tabs(props) {
           tabBarIcon: ({ focused }) => (
             <IconButton
               icon="home-outline"
-              // color="#BFD9CD"
               color="black"
-              // size={12}
             />
           ),
         }}
@@ -53,9 +43,7 @@ function Tabs(props) {
           tabBarIcon: ({ focused }) => (
             <IconButton
               icon="account-circle-outline"
-              // color="#BFD9CD"
               color="black"
-              // size={12}
             />
           ),
         }}
@@ -67,9 +55,7 @@ function Tabs(props) {
           tabBarIcon: ({ focused }) => (
             <IconButton
               icon="format-list-checkbox"
-              // color="#BFD9CD"
               color="black"
-              // size={12}
             />
           ),
         }}
@@ -81,9 +67,7 @@ function Tabs(props) {
           tabBarIcon: ({ focused }) => (
             <IconButton
               icon="bell-outline"
-              // color="#BFD9CD"
               color="black"
-              // size={12}
             />
           ),
         }}
@@ -104,62 +88,3 @@ const styles = StyleSheet.create({
 });
 
 export default Tabs;
-
-// import React from "react";
-// import { StyleSheet, View, Text, Pressable } from "react-native";
-// import { Entypo } from "@expo/vector-icons";
-// import { IconButton, Colors } from 'react-native-paper';
-// import colors from "../config/colors";
-
-// var SIZE=120
-// var RADIUS=SIZE/2
-// var ICONSIZE=SIZE/2
-
-// function Tabs(props) {
-//    return (
-//       <View  style={styles.circle}>
-//          {/* <Entypo name={props.iconName} size={40} color="black"></Entypo> */}
-//          <IconButton
-//           icon={props.iconName}
-//           // color="#BFD9CD"
-//           color="black"
-//           size={ICONSIZE}
-//         />
-//         <View style={{bottom: 15}}>
-
-//          <Text style={styles.pressableText}>{props.text}</Text>
-//         </View>
-//       </View>
-//    );
-// }
-
-// const styles = StyleSheet.create({
-//    circle: {
-//       backgroundColor: colors.shapeBackground,
-//       // backgroundColor: "white",
-//       // opacity: 0.9,
-//       width: SIZE,
-//       height: SIZE,
-//       borderRadius: RADIUS,
-//       alignItems: "center",
-//       justifyContent: "center",
-//    },
-//    pressableText: {
-//       color: "black",
-//       fontSize: 10,
-//       // fontWeight: "bold",
-//       //borderRadius: 50
-//       //alignContent:'center',
-//       //justifyContent: 'center',
-//    },
-//    bigCircle:{
-//     backgroundColor: colors.shapeBackground,
-//     width: 110,
-//     height: 110,
-//     borderRadius: 55,
-//     alignItems: "center",
-//     justifyContent: "center",
-//    },
-// });
-
-// export default Tabs;
