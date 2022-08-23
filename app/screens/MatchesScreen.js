d3f03b06d87e96e5cb28957918dbf130058f4a59
HilaIlarov <hilailarov@gmail.com>
2022-08-23 12:14:18 +0300

    Fixed matches screen

--- app/screens/MatchesScreen.js
+++ app/screens/MatchesScreen.js
@@ -229,7 +229,7 @@ const styles = StyleSheet.create({
     backgroundColor: "white",
     height: "100%",
     width: "100%",
-    top: "20%",
+    top: "22%",
     flexDirection: "column",
     alignContent: "center",
     justifyContent: "center",
