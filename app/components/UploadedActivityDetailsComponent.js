fc899a7f746e9bcd07cd37a5bae48c93a5860920
HilaIlarov <hilailarov@gmail.com>
2022-08-23 11:10:40 +0300

    Fixed UI in activity details

--- app/components/UploadedActivityDetailsComponent.js
+++ app/components/UploadedActivityDetailsComponent.js
@@ -86,13 +86,17 @@ export default function UploadedActivityDetailsComponent(props) {
 							<View
 								style={{
 									flexDirection: "row",
-									alignItems: "center",
+									// alignItems: "center",
+									flexDirection: "row",
+									alignItems: "flex-start",
 								}}
 							>
 								<Text style={styles.titlesStyle}>Location: </Text>
-								<Text style={styles.activityDetailsText}>
-									{DATA.location}
-								</Text>
+								<View style={{ width: "60%" }}>
+									<Text style={styles.activityDetailsText}>
+										{DATA.location}
+									</Text>
+								</View>
 							</View>
 						</View>
 						{/*activity date*/}
@@ -237,7 +241,7 @@ const styles = StyleSheet.create({
 	},
 	location: {
 		// top: "10%",
-		marginRight: "15%",
+		// marginRight: "15%",
 	},
 	date: {
 		// borderWidth: 1,
